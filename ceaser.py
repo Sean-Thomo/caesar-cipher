41ec01d7d06a2d11b56047809aceb179925c0fbf
sithomo022 <sithomo022@student.wethinkcode.co.za>
2022-11-11 16:47:42 +0200

    shift alphabets and cipher

--- ceaser.py
+++ ceaser.py
@@ -1,15 +1,26 @@
+import string
+
 def get_user_input():
-		plaintext = input("Enter Text: ")
-		return plaintext
-  
-  
+	plaintext = input("Enter Text: ")
+	return plaintext
+
+
 def get_key():
-  key = input("Enter Key: ")
+  key = int(input("Enter Key: "))
   return key
 
 
 def cipher(plaintext, key):
-  pass
+  cipher_text = ""
+  alphabet = string.ascii_lowercase
+  shifted_alphabet = string.ascii_lowercase[key:] + string.ascii_lowercase[0:key]
+  key_value_alphabet = {key: value for key, value in zip(alphabet, shifted_alphabet)}
+  for i in plaintext:
+    cipher_char = key_value_alphabet[i]
+    cipher_text += cipher_char
+
+  print(cipher_text)
+
 
 def encrypt():
   plaintext = get_user_input()
